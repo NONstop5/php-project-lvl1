dc9f86b4676ae8263b39b4e8121602cebd969a5a
NONstop <non_2002@mail.ru>
2021-11-14 21:55:50 +0300

    fix Cli.php to function can return name

--- src/Cli.php
+++ src/Cli.php
@@ -7,9 +7,11 @@ namespace App\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome(): void
+function welcome(): string
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    return $name;
 }
