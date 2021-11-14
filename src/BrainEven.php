d07e263d19dfe4805e618b0a3579f604dc6a2c58
NONstop <non_2002@mail.ru>
2021-11-14 21:56:15 +0300

    add BrainEven.php

---
+++ src/BrainEven.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\BrainEven;
+
+use function App\Cli\welcome;
+use function cli\line;
+use function cli\prompt;
+
+function even(): void
+{
+    $answerEvenMap = [
+        true => 'yes',
+        false => 'no',
+    ];
+    $name = welcome();
+
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+
+    for ($i = 1; $i <= 3; $i++) {
+        $number = rand();
+        line("Question: {$number}");
+        $answer = prompt('Your answer');
+
+        $isEven = $number % 2 === 0;
+
+        if (($answer === 'yes' && $isEven) || ($answer === 'no' && !$isEven)) {
+            line('Correct!');
+            continue;
+        }
+
+        exit("'{$answer}' is wrong answer ;(. Correct answer was '{$answerEvenMap[$isEven]}'." . PHP_EOL . "Let's try again, {$name}!" . PHP_EOL);
+    }
+
+    line("Congratulations, {$name}!");
+}
