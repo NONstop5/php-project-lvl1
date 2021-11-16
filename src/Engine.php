f342677bc5d7cf759a002b1a30e48edb6ba2ca0d
NONstop <non_2002@mail.ru>
2021-11-16 13:07:18 +0300

    refactor files to change namespaces and move duplicate logics to Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -4,13 +4,36 @@ declare(strict_types=1);
 
 namespace App\Engine;
 
+use function cli\line;
+use function cli\prompt;
+
 const ROUND_COUNT = 3;
 const MAX_NUMBER = 100;
 
-function exitWithText($answer, $correctAnswer, $name): void
+function exitWithText(string $answer, string $correctAnswer, string $name): void
 {
     exit(
         "'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'." .
         PHP_EOL . "Let's try again, {$name}!" . PHP_EOL
     );
+}
+
+function showCongratulation(string $name): void
+{
+    line("Congratulations, {$name}!");
+}
+
+function showQuestion(string $question): void
+{
+    line("Question: {$question}");
+}
+
+function showOkText(): void
+{
+    line('Correct!');
+}
+
+function getAnswer(): string
+{
+    return prompt('Your answer');
 }
