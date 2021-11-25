a95916094059bbedf38b3232df5193c20b9c98fd
NONstop <non_2002@mail.ru>
2021-11-25 23:55:12 +0300

    fix BrainProgression.php to move logic to separate function

--- src/Games/BrainProgression.php
+++ src/Games/BrainProgression.php
@@ -4,60 +4,66 @@ declare(strict_types=1);
 
 namespace App\Games\BrainProgression;
 
+use function App\Cli\welcome;
 use function App\Engine\exitWithText;
-use function App\Engine\getAnswer;
 use function App\Engine\showCongratulation;
 use function App\Engine\showOkText;
 use function App\Engine\showQuestion;
-use function App\Games\Cli\welcome;
 use function cli\line;
 
 use const App\Engine\ROUND_COUNT;
 
+const PROGRESSION_MIN_COUNT = 5;
+const PROGRESSION_MAX_COUNT = 10;
+const PROGRESSION_MIN_STEP = 1;
+const PROGRESSION_MAX_STEP = 5;
+
 function progression(): void
 {
-    $progressionMinCount = 5;
-    $progressionMaxCount = 10;
-    $progressionMinStep = 1;
-    $progressionMaxStep = 5;
-
     $name = welcome();
 
     line('What number is missing in the progression?');
 
     for ($i = 1; $i <= ROUND_COUNT; $i++) {
-        $numbers = [];
-        $answerNumber = null;
-        $progressionCount = rand($progressionMinCount, $progressionMaxCount);
-        $progressionStep = rand($progressionMinStep, $progressionMaxStep);
-        $progressionMissingNumber = rand(1, $progressionCount);
-        $progressionStartValue = rand(1, ($progressionCount * $progressionMaxStep));
+        [$progression, $correctAnswer] = getProgressionData();
 
-        $number = $progressionStartValue;
+        $answer = showQuestion($progression);
 
-        for ($n = 1; $n <= $progressionCount; $n++) {
-            if ($n === $progressionMissingNumber) {
-                $numbers[] = '..';
-                $answerNumber = $number;
-            } else {
-                $numbers[] = $number;
-            }
-
-            $number += $progressionStep;
-        }
-
-        $question = implode(' ', $numbers);
-
-        showQuestion($question);
-        $answer = getAnswer();
-
-        if ($answerNumber === (int)$answer) {
+        if ($correctAnswer === (int)$answer) {
             showOkText();
             continue;
         }
 
-        exitWithText($answer, (string)$answerNumber, $name);
+        exitWithText($answer, (string)$correctAnswer, $name);
     }
 
     showCongratulation($name);
+}
+
+function getProgressionData(): array
+{
+    $numbers = [];
+    $answerNumber = null;
+    $progressionCount = rand(PROGRESSION_MIN_COUNT, PROGRESSION_MAX_COUNT);
+    $progressionStep = rand(PROGRESSION_MIN_STEP, PROGRESSION_MAX_STEP);
+    $progressionMissingNumber = rand(1, $progressionCount);
+    $progressionStartValue = rand(1, ($progressionCount * PROGRESSION_MAX_STEP));
+
+    $number = $progressionStartValue;
+
+    for ($n = 1; $n <= $progressionCount; $n++) {
+        if ($n === $progressionMissingNumber) {
+            $numbers[] = '..';
+            $answerNumber = $number;
+        } else {
+            $numbers[] = $number;
+        }
+
+        $number += $progressionStep;
+    }
+
+    return [
+        implode(' ', $numbers),
+        $answerNumber,
+    ];
 }
