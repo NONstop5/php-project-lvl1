6c8c79f75c8b7733e6a01d94a1993dce8a397ccb
IgorIlyaguev <non_2002@mail.ru>
2024-03-12 20:23:15 +0300

    fix Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -9,9 +9,6 @@ use function cli\prompt;
 
 function welcome()
 {
-    if (false) {
-        $asd = 1;
-    }
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
