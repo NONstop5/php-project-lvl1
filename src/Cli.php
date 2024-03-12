3529c43478083dc7f3e7e69b65ac06833d2050ab
IgorIlyaguev <non_2002@mail.ru>
2024-03-12 19:52:23 +0300

    fix Cli.php to change text

--- src/Cli.php
+++ src/Cli.php
@@ -11,7 +11,7 @@ function welcome()
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Hello1, %s!", $name);
 
     return $name;
 }
