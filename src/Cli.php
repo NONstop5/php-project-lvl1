1eb3d5239962a2b75e62c721536f38dfc6c5b68d
IgorIlyaguev <non_2002@mail.ru>
2024-03-12 19:58:47 +0300

    fix Cli.php to change text

--- src/Cli.php
+++ src/Cli.php
@@ -9,9 +9,12 @@ use function cli\prompt;
 
 function welcome()
 {
+    if (false) {
+        $asd = 1;
+    }
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
-    line("Hello1, %s!", $name);
+    line("Hello, %s!", $name);
 
     return $name;
 }
